a63b2867e6b1a9b68dc66667b33e3c08827c7d1b
BKR70 <bilas.duet.204027@gmail.com>
2025-06-11 17:40:58 +0600

    Value vs Ref. type added

---
+++ Value vs Ref type/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Person {
+    public string name; 
+    public int age;
+    public Person(string n,int ag) {
+
+        //Console.WriteLine("Default Constructor is called");
+        name = n; age = ag;
+    }
+}
+class Test {
+    public static void Main(string[] args) {
+
+        Person p1 = new Person("Bilas",25);
+        Console.WriteLine($"Before refering P1.name is {p1.name}, P1.age is {p1.age}.");
+        Console.WriteLine();
+
+        Person p2 = p1;
+        p2.name = "Virat Kohli";
+        p2.age = 29;
+
+        Console.WriteLine($"P2.name is {p2.name}, P2.age is {p2.age}.");
+        Console.WriteLine($"After refering P1.name is {p1.name}, P1.age is {p1.age}.");
+
+        Console.ReadKey();
+    }
+}
