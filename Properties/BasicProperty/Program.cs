7a0c4221566c963fa3735c697fc1363b624968ba
BKR70 <bilas.duet.204027@gmail.com>
2025-06-13 12:11:41 +0600

    new Properties file added

---
+++ Properties/BasicProperty/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProperty
+{
+    public class Person
+    {
+        private int age;
+        private string name;
+        public int Age
+        {              // Property or indexer cannot have void type.
+            get { return age; }
+            set
+            {
+                if (value > 17)
+                {
+                    age = value;
+                }
+            }
+
+            //set{age = value;}   
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+            // or               
+            // get; set;             // This can only be done when there is absolutely no customization.
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Age = 14;
+            p1.Name = "Bilas Kumar";
+            Console.WriteLine($"Name : {p1.Name}. \nAge : {p1.Age}");
+        }
+    }
+}
