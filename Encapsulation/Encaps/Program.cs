4ea5f9c9d7764cee35c0daf742d8a87daeb4e555
BKR70 <bilas.duet.204027@gmail.com>
2025-06-15 00:05:48 +0600

    Encapsulation added

---
+++ Encapsulation/Encaps/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+  Encapsulation is a process of
+     01. packaging variables and methods into a single unit.
+     02. protecting data by declaring them as private.
+*/
+
+using System;
+
+namespace Encaps
+{
+    public class Student
+    {
+        private String studentName;
+        private int studentAge;
+        public String StudentName
+        {
+            get { return studentName; }
+            set { studentName = value; }
+        }
+        public int StudentAge
+        {
+            get { return studentAge; }
+            set { studentAge = value; }
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj = new Student();
+            obj.StudentName = "Bilas";
+            obj.StudentAge = 25;
+
+            Console.WriteLine(" Name: " + obj.StudentName);
+            Console.WriteLine(" Age: " + obj.StudentAge);
+        }
+    }
+}
